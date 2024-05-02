cda72e10234b58d990dfe238ebdc9464c45c9bfd
MiguelC29 <miguelito28c@gmail.com>
2024-05-01 22:04:31 -0500

    aula 01 - inmersion java

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos a la inmersión en Java!");
+        // System.out.println("Película Matrix");
+
+        // Declaración de variables
+        String nombre = "Matrix";
+        String sinopsis = """
+                La mejor película del fin del milenio
+                """;
+        int fechaDeLanzamiento = 1999;
+        double evaluacion = 4.5;
+        boolean incluidoEnElPlanBasico = true;
+        double mediaEvaluacionUsuario = 0;
+
+        System.out.println("Película: " + nombre);
+        System.out.println("Fecha de lanzamiento: " + fechaDeLanzamiento);
+        System.out.println("Calificación: " + evaluacion);
+        System.out.println("Incluido en el plan básico: " + incluidoEnElPlanBasico);
+
+        double mediaEvaluacion = (4.5 + 4.8 + 3) / 3;
+        System.out.println("Media de la evaluación de Matrix: " + mediaEvaluacion);
+
+        if (fechaDeLanzamiento >= 2023) {
+            System.out.println("Película Popular en el momento");
+        } else {
+            System.out.println("Pelíula Retro que vale la pena ver");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            Scanner teclado = new Scanner(System.in);
+            System.out.print("Ingresa la nota que le darías a Matrix: ");
+            double notaMatrix = teclado.nextDouble();
+            // mediaEvaluacion = mediaEvaluacionUsuario + notaMatrix;
+            mediaEvaluacionUsuario += notaMatrix;
+        }
+        System.out.println("La media de la película Matrix calculada por el usuario es: " + (mediaEvaluacionUsuario / 3));
+    }
+}
