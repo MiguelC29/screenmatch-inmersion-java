e84a92d153d4ade1dd2bfe78d542d9e626e0d56d
MiguelC29 <miguelito28c@gmail.com>
2024-05-02 16:46:10 -0500

    implementando ciclo while para calcular la media

--- src/Main.java
+++ src/Main.java
@@ -29,13 +29,30 @@ public class Main {
             System.out.println("Pelíula Retro que vale la pena ver");
         }
 
+        // Ciclo FOR
+        Scanner teclado = new Scanner(System.in);
         for (int i = 0; i < 3; i++) {
-            Scanner teclado = new Scanner(System.in);
             System.out.print("Ingresa la nota que le darías a Matrix: ");
             double notaMatrix = teclado.nextDouble();
             // mediaEvaluacion = mediaEvaluacionUsuario + notaMatrix;
             mediaEvaluacionUsuario += notaMatrix;
         }
         System.out.println("La media de la película Matrix calculada por el usuario es: " + (mediaEvaluacionUsuario / 3));
+
+        // Ciclo WHILE
+        int iterador = 0;
+        int cantidadDeCalificaciones;
+
+        System.out.print("\nCuántas calificaciones desea ingresar: ");
+        cantidadDeCalificaciones = teclado.nextInt();
+        mediaEvaluacion = 0;
+
+        while (iterador < cantidadDeCalificaciones) {
+            System.out.print("Ingresa la calificación que le darías a Matrix: ");
+            double calificacionMatrix = teclado.nextDouble();
+            mediaEvaluacion += calificacionMatrix;
+            iterador++;
+        }
+        System.out.println("La media de la película Matrix calculada por el usuario es: " + (mediaEvaluacion / cantidadDeCalificaciones));
     }
 }
