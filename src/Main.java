38d6426cf809f490e9e7e2ac6947ff99d24e9f84
MiguelC29 <miguelito28c@gmail.com>
2024-05-03 22:27:40 -0500

    aula 02 - inmersion java

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,25 @@
-import java.util.Scanner;
+import modelo.Pelicula;
+import modelo.Serie;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenidos a la inmersión en Java!");
-        // System.out.println("Película Matrix");
+        // Tipos de datos de referencia
+//        Pelicula matrix = new Pelicula();
+//        matrix.setNombre("Matrix");
+//        matrix.setTiempoDeDuracionEnMinutos(120);
+//        matrix.setFechaDeLanzamiento(1999);
+//
+//        matrix.muestraFichaTecnica();
+//
+//        Serie friends = new Serie();
+//        friends.setNombre("Friends");
+//        friends.setFechaDeLanzamiento(1996);
+//        friends.setTemporadas(10);
+//        friends.setEpisodiosPorTemporada(22);
+//        friends.setDuracionEnMinutosPorEpisodio(30);
+//        friends.muestraFichaTecnica();
 
-        // Declaración de variables
-        String nombre = "Matrix";
-        String sinopsis = """
-                La mejor película del fin del milenio
-                """;
-        int fechaDeLanzamiento = 1999;
-        double evaluacion = 4.5;
-        boolean incluidoEnElPlanBasico = true;
-        double mediaEvaluacionUsuario = 0;
-
-        System.out.println("Película: " + nombre);
-        System.out.println("Fecha de lanzamiento: " + fechaDeLanzamiento);
-        System.out.println("Calificación: " + evaluacion);
-        System.out.println("Incluido en el plan básico: " + incluidoEnElPlanBasico);
-
-        double mediaEvaluacion = (4.5 + 4.8 + 3) / 3;
-        System.out.println("Media de la evaluación de Matrix: " + mediaEvaluacion);
-
-        if (fechaDeLanzamiento >= 2023) {
-            System.out.println("Película Popular en el momento");
-        } else {
-            System.out.println("Pelíula Retro que vale la pena ver");
-        }
-
-        // Ciclo FOR
-        Scanner teclado = new Scanner(System.in);
-        for (int i = 0; i < 3; i++) {
-            System.out.print("Ingresa la nota que le darías a Matrix: ");
-            double notaMatrix = teclado.nextDouble();
-            // mediaEvaluacion = mediaEvaluacionUsuario + notaMatrix;
-            mediaEvaluacionUsuario += notaMatrix;
-        }
-        System.out.println("La media de la película Matrix calculada por el usuario es: " + (mediaEvaluacionUsuario / 3));
-
-        // Ciclo WHILE
-        int iterador = 0;
-        int cantidadDeCalificaciones;
-
-        System.out.print("\nCuántas calificaciones desea ingresar: ");
-        cantidadDeCalificaciones = teclado.nextInt();
-        mediaEvaluacion = 0;
-
-        while (iterador < cantidadDeCalificaciones) {
-            System.out.print("Ingresa la calificación que le darías a Matrix: ");
-            double calificacionMatrix = teclado.nextDouble();
-            mediaEvaluacion += calificacionMatrix;
-            iterador++;
-        }
-        System.out.println("La media de la película Matrix calculada por el usuario es: " + (mediaEvaluacion / cantidadDeCalificaciones));
+        Principal principal = new Principal();
+        principal.muestraElMenu();
     }
 }
