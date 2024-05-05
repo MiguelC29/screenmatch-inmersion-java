4d1ba7210a0371c3af98c0f521a4027a381a339a
MiguelC29 <miguelito28c@gmail.com>
2024-05-05 13:46:27 -0500

    tutoria - añadir series y calcular tiempo para maratonear

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,15 @@
+import calculos.CalculadoraDeTiempo;
 import modelo.Pelicula;
+import modelo.Serie;
 
 import java.util.Scanner;
 
 public class Principal {
+
+    Pelicula peliculaUsuario = new Pelicula();
+    Serie serieUsuario = new Serie();
+    CalculadoraDeTiempo calculadoraDeTiempo = new CalculadoraDeTiempo();
+
     public void muestraElMenu() {
         int opcion = 0;
         Scanner teclado = new Scanner(System.in);
@@ -12,7 +19,7 @@ public class Principal {
                     Bienvenido(a) a Screenmatch
                     1) Registrar nueva película
                     2) Registrar nueva serie
-                    
+                    3) Calculadora de tiempo
                     9) Salir
                     """;
 
@@ -32,11 +39,44 @@ public class Principal {
                     System.out.println("Ingrese la duración en minutos de la película");
                     int duracionEnMinutos = teclado.nextInt();
 
-                    Pelicula peliculaUsuario = new Pelicula();
                     peliculaUsuario.setNombre(nombre);
                     peliculaUsuario.setFechaDeLanzamiento(fechaDeLanzamiento);
                     peliculaUsuario.setTiempoDeDuracionEnMinutos(duracionEnMinutos);
                     peliculaUsuario.muestraFichaTecnica();
+
+                    calculadoraDeTiempo.incluye(peliculaUsuario);
+                    break;
+                case 2:
+                    System.out.println("Ingrese el nombre de la serie");
+                    String nombreSerie = teclado.nextLine();
+
+                    System.out.println("Ingrese el año de lanzamiento de la serie");
+                    int fechaDeLanzamientoSerie = teclado.nextInt();
+                    teclado.nextLine();
+
+                    System.out.println("Por favor ingresa el número de temporadas para esta serie");
+                    int temporadas = teclado.nextInt();
+                    teclado.nextLine();
+
+                    System.out.println("Por favor ingresa la cantidad de episodios por cada temporada");
+                    int episodiosPorTemporada = teclado.nextInt();
+                    teclado.nextLine();
+
+                    System.out.println("Por favor ingresa la duración en minutos de cada episodio");
+                    int minutosPorEpisodio = teclado.nextInt();
+                    teclado.nextLine();
+
+                    serieUsuario.setNombre(nombreSerie);
+                    serieUsuario.setFechaDeLanzamiento(fechaDeLanzamientoSerie);
+                    serieUsuario.setTemporadas(temporadas);
+                    serieUsuario.setEpisodiosPorTemporada(episodiosPorTemporada);
+                    serieUsuario.setDuracionEnMinutosPorEpisodio(minutosPorEpisodio);
+                    serieUsuario.muestraFichaTecnica();
+
+                    calculadoraDeTiempo.incluye(serieUsuario);
+                    break;
+                case 3:
+                    System.out.println("Tiempo que necesitas para maratonar tus títulos favoritos " + calculadoraDeTiempo.getTiempoTotal() + " minutos");
                     break;
                 case 9:
                     System.out.println("Saliendo del programa");
---
+++ src/calculos/CalculadoraDeTiempo.java
@@ -0,0 +1,15 @@
+package calculos;
+
+import modelo.Titulo;
+
+public class CalculadoraDeTiempo {
+    private int tiempoTotal;
+
+    public int getTiempoTotal() {
+        return tiempoTotal;
+    }
+
+    public void incluye(Titulo titulo) {
+        tiempoTotal += titulo.getTiempoDeDuracionEnMinutos();
+    }
+}
